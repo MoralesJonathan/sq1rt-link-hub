a647d63f87927faf6a6c88c018e3a8d0d75fbd65
Jonathan Morales <jonmorazav@yahoo.com>
2020-09-29 17:09:19 -0400

    re-order links

--- src/data/links.ts
+++ src/data/links.ts
@@ -9,20 +9,12 @@ export interface Link {
   
   const links: Link[] = [
     {
-      title: 'Revolution 93.5 Mix 1',
-      subTitle: 'Aired: 06/09/20',
-      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
-      url: 'https://sq1rt.me/rev-mix',
-      cta: 'Play',
+      title: 'Dirtybird DJ Contest',
+      subTitle: 'Vote on OnNow.TV for SQ!RT',
+      icon: process.env.PUBLIC_URL + '/assets/onNowTv.png',
+      url: 'https://sq1rt.me/dirtybirdcontest',
+      cta: 'VOTE',
       id: 0
-    },
-    {
-      title: 'Revolution 93.5 Mix 2',
-      subTitle: 'Aired: 06/30/20',
-      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
-      url: 'https://sq1rt.me/rev-mix-2',
-      cta: 'Play',
-      id: 1
     },
     {
       title: 'Revolution 93.5 Mix 3',
@@ -30,14 +22,22 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
       url: 'https://sq1rt.me/rev-mix-3',
       cta: 'Play',
+      id: 1
+    },
+    {
+      title: 'Revolution 93.5 Mix 2',
+      subTitle: 'Aired: 06/30/20',
+      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
+      url: 'https://sq1rt.me/rev-mix-2',
+      cta: 'Play',
       id: 2
     },
     {
-      title: 'Dirtybird DJ Contest',
-      subTitle: 'Vote on OnNow.TV for SQ!RT',
-      icon: process.env.PUBLIC_URL + '/assets/onNowTv.png',
-      url: 'https://www.onnow.tv/competition/dirtybird-couchout-2020/',
-      cta: 'VOTE',
+      title: 'Revolution 93.5 Mix 1',
+      subTitle: 'Aired: 06/09/20',
+      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
+      url: 'https://sq1rt.me/rev-mix',
+      cta: 'Play',
       id: 3
     }
   ];
