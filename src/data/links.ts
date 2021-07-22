d16f2912dab31ff55f85287b6b4568f919e2e5ec
Jonathan Morales <jonmorazav@yahoo.com>
2021-07-22 14:46:20 -0400

    udpated links

--- src/data/links.ts
+++ src/data/links.ts
@@ -9,36 +9,20 @@ export interface Link {
   
   const links: Link[] = [
     {
-      title: 'GJWHF x Who x Boss dem SQ!RT Mashup',
-      subTitle: 'Girls Just wanna have fun X Who x Boss Dem SQ!RT Mashup',
-      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
-      url: 'https://soundcloud.com/sq1rt/gjwhf-x-who-x-boss-dem',
-      cta: 'Play',
+      title: 'FEEL Presave',
+      subTitle: 'Presave my newest single on Spotify!',
+      icon: process.env.PUBLIC_URL + '/assets/feel.png',
+      url: 'gate.fm/wKJC2Fm',
+      cta: 'Presave',
       id: 0
     },
     {
-      title: 'Lemonade X Show me love SQ!RT Mashup',
-      subTitle: 'Lemonade X Show me love SQ!RT Mashup',
+      title: 'SQ!RT Mashup Pack Vol 1',
+      subTitle: 'Free Volume 1 Mashup Pack',
       icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
-      url: 'https://sq1rt.me/lemonade-show-me-love-mashup',
-      cta: 'Play',
+      url: 'https://sq1rt.me/mashups-vol-1',
+      cta: 'Download',
       id: 1
-    },
-    {
-      title: 'Jumpin X No Mercy X Losing it X Anything Mashup',
-      subTitle: 'Jumpin X No Mercy X Losing it X Anything SQ!RT Mashup',
-      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
-      url: 'https://sq1rt.me/jumpin-mashup',
-      cta: 'Play',
-      id: 2
-    },
-    {
-      title: 'On my mind x Heads will roll Mashup',
-      subTitle: 'On my mind x heads will roll SQ!RT mashup',
-      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
-      url: 'https://sq1rt.me/on-my-mind-heads-will-roll-mashup',
-      cta: 'Play',
-      id: 3
     },
     {
       title: '2021 Mix',
@@ -46,7 +30,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
       url: 'https://sq1rt.me/2021-mix',
       cta: 'Play',
-      id: 4
+      id: 2
     },
     {
       title: 'Revolution 93.5 Mix 3',
@@ -54,7 +38,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
       url: 'https://sq1rt.me/rev-mix-3',
       cta: 'Play',
-      id: 5
+      id: 3
     },
   ];
   
