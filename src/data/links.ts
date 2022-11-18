b31c94187aa078dc2a9800253787c9b8aa0620bf
Jonathan Morales <moralesjonathan@email.com>
2022-11-18 13:25:38 -0500

    add remix

--- src/data/links.ts
+++ src/data/links.ts
@@ -9,12 +9,20 @@ export interface Link {
   
   const links: Link[] = [
     {
-      title: 'Slow down Amsterdam',
-      subTitle: 'Listen to my latest mashup".',
-      icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
-      url: 'https://sq1rt.me/slow-down-amsterdam',
+      title: 'Gimme some keys remix',
+      subTitle: 'My remix of Matroda\'s "Gimme some keys"',
+      icon: process.env.PUBLIC_URL + '/assets/gimmeKeys.jpg',
+      url: 'https://sq1rt.me/gimme-some-keys',
       cta: 'Listen now',
       id: 0
+    },
+    {
+      title: 'Slow down Amsterdam',
+      subTitle: 'Listen to my latest mashup',
+      icon: process.env.PUBLIC_URL + '/assets/amsterdamnDrugs.jpg',
+      url: 'https://sq1rt.me/slow-down-amsterdam',
+      cta: 'Listen now',
+      id: 1
     },
     {
       title: 'Moscow Mule EDIT',
@@ -22,7 +30,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/moscowMule.png',
       url: 'https://sq1rt.me/moscow-mule',
       cta: 'Listen now',
-      id: 1
+      id: 2
     },
     {
       title: 'SQ!RT Mashup Pack Vol 1',
@@ -30,7 +38,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
       url: 'https://sq1rt.me/mashups-vol-1',
       cta: 'Download',
-      id: 2
+      id: 3
     },
     {
       title: 'GET NASTY',
@@ -38,7 +46,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/GetNasty.png',
       url: 'https://gate.fm/W9TcTC',
       cta: 'STREAM',
-      id: 3
+      id: 4
     }
   ];
   
