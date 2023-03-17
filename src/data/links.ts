785e9bb7b9cef3043dd64017ea0f58539a88d9f6
Jonathan Morales <moralesjonathan@email.com>
2023-03-17 11:53:34 -0400

    add alone again link

--- src/data/links.ts
+++ src/data/links.ts
@@ -9,12 +9,20 @@ export interface Link {
   
   const links: Link[] = [
     {
+      title: 'Alone again (Remix)',
+      subTitle: 'Listen to my remix of "Alone again" on Youtube',
+      icon: process.env.PUBLIC_URL + '/assets/aloneAgain.jpg',
+      url: 'https://sq1rt.me/alone-again-yt',
+      cta: 'LISTEN',
+      id: 0
+    },
+    {
       title: 'Front To Back',
       subTitle: 'Stream my latest single "Front To Back"!',
       icon: process.env.PUBLIC_URL + '/assets/FrontToBack.jpeg',
       url: 'https://gate.fm/eBePRQpnh',
       cta: 'STREAM',
-      id: 0
+      id: 1
     },
     {
       title: 'Gimme some keys remix',
@@ -22,7 +30,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/gimmeKeys.jpg',
       url: 'https://sq1rt.me/gimme-some-keys',
       cta: 'Listen now',
-      id: 1
+      id: 2
     },
     {
       title: 'Slow down Amsterdam',
@@ -30,7 +38,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/amsterdamnDrugs.jpg',
       url: 'https://sq1rt.me/slow-down-amsterdam',
       cta: 'Listen now',
-      id: 2
+      id: 3
     },
     {
       title: 'Moscow Mule EDIT',
@@ -38,7 +46,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/moscowMule.png',
       url: 'https://sq1rt.me/moscow-mule',
       cta: 'Listen now',
-      id: 3
+      id: 4
     },
     {
       title: 'SQ!RT Mashup Pack Vol 1',
@@ -46,7 +54,7 @@ export interface Link {
       icon: process.env.PUBLIC_URL + '/assets/soundcloud.png',
       url: 'https://sq1rt.me/mashups-vol-1',
       cta: 'Download',
-      id: 4
+      id: 5
     },
   ];
   
